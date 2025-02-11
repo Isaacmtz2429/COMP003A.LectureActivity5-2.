97c6098e9e4151c79064d61a2cc8eb335dcf2bb1
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-11 14:58:27 -0800

    Add project files.

---
+++ COMP003A.LectureActivity5-2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP003A
+// Faculty: Jonathan Cruz
+// Purpose: Demonstrate encapsulation using private fields and properties in C#
+*/
+namespace COMP003A.LectureActivity5_2_
+{
+    internal class BankAccount
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
